04da1da9147977a67dba0151a750891d53dbc25e
syedanashraghaffar <work.nashra@gmail.com>
2022-03-11 22:00:01 +0500

    Code Updated

---
+++ app.js
@@ -0,0 +1,53 @@
+//Synchronus Javascript
+//---------------------
+
+
+// ONE INPUT WAY
+// 1. prompt
+
+//THREE OUTPUT WAYS
+// 1. alert
+// 2. console
+// 3. document.write
+
+
+////////////////////////////////////////////////// CLASS 1 JS /////////////////////////////////////////////////////
+
+//Variables
+
+var x = 5; 
+var str = "Hello World";
+
+
+// Output
+
+// alert("Hello")
+
+// document.write("hello")
+
+// console.log("hello")
+
+//Input
+
+// var userInput = prompt("Enter your name","My name is ...")
+// alert("Hello " + userInput);
+// console.log(userInput)
+
+
+// var userInput = +prompt("Enter any number..");
+// console.log(userInput);
+// var sum = 2 + userInput;
+// console.log(sum);
+
+
+// Inc and Dec Op
+
+// pre inc
+// var a = 5;
+// alert(++a);
+
+
+// post inc
+// var a = 5;
+// alert(a++);
+// alert(a);
